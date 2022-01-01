28282d6554cb9caa7cb01accd96d9249c5843519
jgpark316 <70467140+romeo316@users.noreply.github.com>
2022-01-01 21:46:10 +0900

    Delete HellowWorld.java

--- src/HellowWorld.java
+++
@@ -1,8 +0,0 @@
-
-public class HellowWorld {
-
-		public static void main(String[] args) {
-			System.out.println("흑푸들 해리의 첫 자바 도전기~~ ~~ Hellow World!!!");
-			System.out.println("흑푸들 해리의 첫 자바 도전기~~ ~~ Hellow World!!!2222");
-		}
-}
